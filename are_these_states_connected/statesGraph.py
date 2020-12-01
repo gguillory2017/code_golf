dad398b713692534d7c1aedf261560c0e615449b
Greg <gguillory2015@gmail.com>
2020-12-01 04:46:47 -0600

    Are these states connected
    Added code for filtering input, printing removed states

--- are_these_states_connected/statesGraph.py
+++ are_these_states_connected/statesGraph.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import scipy.sparse as sp
 
 file_raw = open("states_list.txt").read()
 
@@ -14,17 +15,22 @@ for pair in state_pairs:
     pair_list = pair.split(",")
     state_graph.add_edge(pair_list[0], pair_list[1])
 
-print("Number of nodes in state graph: " + str(state_graph.number_of_nodes()))
-
 # Uncomment the line below to produce a graph of the states
 #nx.draw_kamada_kawai(state_graph, with_labels=True)
-
+'''
 input_states_string = str(input(
     "Enter a list of states below. Use all capital letters, separated by commas"+
     ". Do not end or start the list with a comma.\n"))
+'''
+input_states_string = "LA,AK,CA,FL,OP,QD,ME,XT,TX"
 
 input_states_list = input_states_string.split(",")
+input_states_list_filtered = list(filter(lambda s: s in state_set, input_states_list))
 
-input_states_list = filter(lambda s: s not in state_set, input_states_list)
+if len(input_states_list) != len(input_states_list_filtered):
+    removed_states_set = set(input_states_list).difference(set(input_states_list_filtered))
+    print("Removed states: " + str(removed_states_set))
+    
+a= nx.adjacency_matrix(state_graph)
 
-print(input_states_list)
+print(a.todense())
